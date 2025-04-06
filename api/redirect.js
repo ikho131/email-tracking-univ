987d7e69e1eb759914ed03a895785bd11b2ad431
ikho131 <ikho.3line@gmail.com>
2025-04-07 08:24:24 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,19 +1,21 @@
 export default async function handler(req, res) {
   const { email, link, university, company, type = "click" } = req.query;
 
-  await fetch("https://hooks.zapier.com/hooks/catch/22340938/2cjmhpv/", {
-
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      type,
-      email,
-      link,
-      university,
-      company,
-      time: new Date().toISOString(),
-    }),
-  });
+  // type이 'click'일 때만 Zapier로 전송
+  if (type.toLowerCase() === "click") {
+    await fetch("https://hooks.zapier.com/hooks/catch/22340938/2cjmhpv/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        type,
+        email,
+        link,
+        university,
+        company,
+        time: new Date().toISOString(),
+      }),
+    });
+  }
 
   res.writeHead(302, { Location: decodeURIComponent(link) });
   res.end();
