ad0e1876ff54eb598b3f8a2f53537364d3159a54
ikho131 <ikho.3line@gmail.com>
2025-04-07 16:24:13 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,11 +1,12 @@
 export default async function handler(req, res) {
-  const { email, link, univ, company } = req.query;
+  const { email, link, univ, company, type } = req.query; // ⬅️ type 추가!
 
-  // Zapier로 클릭 정보 전송
-  await fetch("https://hooks.zapier.com/hooks/catch/XXXXX/XXXXX/", {
+  // 📡 Zapier로 클릭 정보 전송
+  await fetch("https://hooks.zapier.com/hooks/catch/22340938/2cjmhpv/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
+      type: type || "click", // 기본값: click
       email,
       link,
       univ,
@@ -14,7 +15,7 @@ export default async function handler(req, res) {
     }),
   });
 
-  // 원래 링크로 리디렉션
+  // 🔁 원래 링크로 리디렉션
   res.writeHead(302, { Location: decodeURIComponent(link) });
   res.end();
 }
