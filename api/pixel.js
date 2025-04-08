cedb17eabbbf1147886add27efc204c07beba04d
ikho131 <ikho.3line@gmail.com>
2025-04-08 19:28:57 +0900

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -1,16 +1,16 @@
 export default async function handler(req, res) {
   const { email, company, university, t } = req.query;
 
-  // ✅ 로그 출력 (Vercel console)
-  console.log("📬 이메일 열람 기록:", {
+  // ✅ 로그 출력
+  console.log("📩 이메일 열람 기록:", {
     university,
     company,
     email,
     time: t,
   });
 
-  // 📩 Google Apps Script Web App으로 열람 정보 전송
-  await fetch("https://script.google.com/macros/s/AKfycbySeShBDW5SlNZVLoYwheeX0-ww9XmXsH9AOYxX_xe-8PtGiUUe_e0lXMsO_VEum4fsAg/exec", {
+  // 📨 Google Apps Script Web App으로 열람 정보 전송
+  await fetch("https://script.google.com/macros/s/AKfycbzfYf0vQcsh_DkjRfxoIrADS-vbdhc5XO48P4xPr4Np8X7owAmqPAcIeiZS8LcSpHzHuw/exec", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
