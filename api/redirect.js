a4d7cd04742ffd2cd652a132882722e20e8d0be0
ikho131 <ikho.3line@gmail.com>
2025-04-08 20:52:24 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { email, univ, company, type } = req.query;
   const link = decodeURIComponent(req.query.link || "");
 
-  await fetch("https://script.google.com/macros/s/AKfycbwHble9tzrRcWq7tGZg8lIJVXK22OCHMNig5HE_bmmzoJamMfwtG2DhIKPg3E3ztsGtkg/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbyqr2ScBox4QWF0CfI7drVvCB-BZZILeyUsDwYb4MzJiOf4cgFuPibY4WdXXk7K8iv9dw/exec", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
