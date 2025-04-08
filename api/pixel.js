9f83ef3b53c2c9350e9e19158c7f66a69a104d9c
ikho131 <ikho.3line@gmail.com>
2025-04-08 18:30:50 +0900

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { email, company, university, t } = req.query;
 
-  // ✅ 로그 출력
+  // ✅ 로그 출력 (Vercel console에서 확인용)
   console.log("📩 이메일 열람 기록:", {
     university,
     company,
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
     time: t,
   });
 
-  // 📨 Google Apps Script Web App으로 열람 정보 전송
-  await fetch("https://script.google.com/macros/s/AKfycbxSLwfmctyzAA2yWMx73tdRhEpPDqai_ZWGg7uEgYnfhdTUCfkwxIFNYZhFsiiLg-JgMQ/exec", {
+  // 📡 Google Apps Script Web App으로 열람 정보 전송
+  await fetch("https://script.google.com/macros/s/AKfycbz8Sbf-AenX10Td2y4cPyZHEfqOD_vJYIZAd_TB7hYDeeq06qWCcDVeNx7LENwWSsI9tA/exec", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     }),
   });
 
-  // 🖼️ 추적용 1픽셀 이미지 응답
+  // 🖼️ 1픽셀 이미지 응답
   const imageBuffer = Buffer.from(
     "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR42mP8xwEAApIBzV+4Z6YAAAAASUVORK5CYII=",
     "base64"
