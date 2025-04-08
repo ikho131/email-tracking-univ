16df0b7555e16308dd1e71b297bbae57f4731571
ikho131 <ikho.3line@gmail.com>
2025-04-08 18:30:26 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,12 +2,12 @@ export default async function handler(req, res) {
   const { email, univ, company, type } = req.query;
   const link = decodeURIComponent(req.query.link || "");
 
-  // 📨 Google Apps Script Web App으로 클릭 정보 전송
-  await fetch("https://script.google.com/macros/s/AKfycbxSLwfmctyzAA2yWMx73tdRhEpPDqai_ZWGg7uEgYnfhdTUCfkwxIFNYZhFsiiLg-JgMQ/exec", {
+  // 📡 Google Apps Script Web App으로 클릭 정보 전송
+  await fetch("https://script.google.com/macros/s/AKfycbz8Sbf-AenX10Td2y4cPyZHEfqOD_vJYIZAd_TB7hYDeeq06qWCcDVeNx7LENwWSsI9tA/exec", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      type: type || "click", // 기본값: click
+      type: type || "click",
       email,
       link,
       univ,
