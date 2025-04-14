e050ffd6cc2f7116da0dc82b74582d73e1a740a5
ikho131 <ikho.3line@gmail.com>
2025-04-15 08:35:17 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,9 +2,9 @@ export default async function handler(req, res) {
   const { email, university, company, type } = req.query;
   const link = decodeURIComponent(req.query.link || "");
 
-  const scriptMap = {
-    "ikho.3line@gmail.com": "https://script.google.com/macros/s/AKfycbwPZeTa9aAlr3scgnGjYcvC9n3Q50bWob2wE13ICtrReze3jNLCQQGr4tGZlWb7J5bNBw/exec"
-  };
+// 발신자가 항상 ikho.3line@gmail.com이므로, 고정값으로 바로 처리
+const scriptUrl = "https://script.google.com/macros/s/AKfycbwPZeTa9aAlr3scgnGjYcvC9n3Q50bWob2wE13ICtrReze3jNLCQQGr4tGZlWb7J5bNBw/exec";
+
 
   const matchedKey = Object.keys(scriptMap).find(key => email.includes(key));
   const scriptUrl = matchedKey ? scriptMap[matchedKey] : null;
