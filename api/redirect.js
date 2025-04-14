fd1441a7b2eaf112c806e8a3e63534b5e4a59557
ikho131 <ikho.3line@gmail.com>
2025-04-15 08:26:20 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,10 +1,9 @@
-// ✅ 최종 redirect.js: 파라미터 통일 + 안전성 강화
 export default async function handler(req, res) {
   const { email, university, company, type } = req.query;
   const link = decodeURIComponent(req.query.link || "");
 
   const scriptMap = {
-     "ikho.3line@gmail.com": "https://script.google.com/macros/s/AKfycbwPZeTa9aAlr3scgnGjYcvC9n3Q50bWob2wE13ICtrReze3jNLCQQGr4tGZlWb7J5bNBw/exec"
+    "ikho.3line@gmail.com": "https://script.google.com/macros/s/AKfycbwPZeTa9aAlr3scgnGjYcvC9n3Q50bWob2wE13ICtrReze3jNLCQQGr4tGZlWb7J5bNBw/exec"
   };
 
   const matchedKey = Object.keys(scriptMap).find(key => email.includes(key));
