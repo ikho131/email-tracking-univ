2721333980c28dd83ea34db7044766340a79d5ec
ikho131 <ikho.3line@gmail.com>
2025-04-16 13:02:10 +0900

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const linkRaw = req.query.link || "";
   const link = decodeURIComponent(decodeURIComponent(linkRaw)); // ✅ 이중 디코딩
 
-  const scriptUrl = "https://script.google.com/macros/s/AKfycbwmec_CUPJBsO-Pg_EaOBphtgLpdHXbmagh6uW7OzSWwJxW4gdsQqZI7jyMJpp-1Olhwg/exec";
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbzMg6Jb9P2YyDFQcs73a5IPT8JOtL0RZ5vjnK91qDtrE9NujUjtX01M4Km-vdxqAUIlsw/exec";
 
   if (!scriptUrl || !link) {
     return res.status(400).json({ error: "Invalid request. Missing scriptUrl or link." });
